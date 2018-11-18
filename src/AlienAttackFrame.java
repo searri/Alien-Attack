2cfcb9ad7b8cc9aada4d30c6d35d94a4c379525a
Rick Sear <yobob.sear@gmail.com>
2018-11-18 10:02:45 -0500

    File reader working

--- src/AlienAttackFrame.java
+++ src/AlienAttackFrame.java
@@ -8,6 +8,7 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 import java.util.*;
+import java.io.*;
 
 public class AlienAttackFrame extends JFrame {
     static final long serialVersionUID = 1L;
@@ -21,7 +22,19 @@ public class AlienAttackFrame extends JFrame {
     private JButton endButton;
     private JPanel optionPanel;
     private Queue<Integer> commandQueue;    //0 - left, 1 - right
-    
+    private int cycleTime;                  //how often the game clock cycles (milliseconds)
+    private int largeAlienStartSpeed;       //how far Large aliens move per cycle
+    private int medAlienStartSpeed;         //how far Medium aliens move per cycle
+    private int smallAlienStartSpeed;       //how far Small aliens move per cycle
+    private int playerSpeed;                //how far the player moves per cycle
+    private int increaseInterval;           //how many cycles until difficulty increases
+    private int increaseSize;               //how many more aliens spawn when difficulty increases
+    private int startMaxAliens;             //starting max number of aliens
+    private int startMinAliens;             //starting min number of aliens
+    private int largeAlienValue;            //point values for each alien class
+    private int medAlienValue;
+    private int smallAlienValue;
+
     public AlienAttackFrame() {
         
         //Housekeeping
@@ -64,8 +77,31 @@ public class AlienAttackFrame extends JFrame {
 
     public void readConfigFile() {
 
-        gameFrameSize = 900;
-
+        try {
+            ArrayList <String> configSettings = new ArrayList<String>();
+            Scanner fileReader = new Scanner(new File("project-2-searri/src/resources/AlienAttack.properties"));
+            while (fileReader.hasNextLine()) {
+                String line = fileReader.nextLine();
+                String[] data = line.split(" ");
+                configSettings.add(data[1]);
+            }
+            fileReader.close();
+            System.out.println(configSettings);
+        } catch(IOException e) {
+            JOptionPane.showMessageDialog(this, "Could not access config file. Starting game with default values.");
+            cycleTime = 200;
+            largeAlienStartSpeed = 20;
+            medAlienStartSpeed = 30;
+            smallAlienStartSpeed = 40;
+            playerSpeed = 40;
+            increaseInterval = 20;
+            increaseSize = 2;
+            startMaxAliens = 4;
+            startMinAliens = 1;
+            largeAlienValue = 50;
+            medAlienValue = 25;
+            smallAlienValue = 10;
+        }
     }
 
     public class ControlListener implements ActionListener {
