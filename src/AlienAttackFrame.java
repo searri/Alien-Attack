dd46b85f86d15264c408dc4fc0fc25ebc1c397b7
Rick Sear <yobob.sear@gmail.com>
2018-11-18 10:32:26 -0500

    Implement game timer

--- src/AlienAttackFrame.java
+++ src/AlienAttackFrame.java
@@ -5,6 +5,7 @@
  *
  */
 import javax.swing.*;
+import javax.swing.Timer;
 import java.awt.*;
 import java.awt.event.*;
 import java.util.*;
@@ -21,7 +22,9 @@ public class AlienAttackFrame extends JFrame {
     private JButton pauseButton;
     private JButton endButton;
     private JPanel optionPanel;
-    private Queue<Integer> commandQueue;    //0 - left, 1 - right
+    private Timer gameTimer;
+
+    private Queue<Integer> commandQueue;    //0 - left, 1 - right, else - do nothing
     private int cycleTime;                  //how often the game clock cycles (milliseconds)
     private int largeAlienStartSpeed;       //how far Large aliens move per cycle
     private int medAlienStartSpeed;         //how far Medium aliens move per cycle
@@ -39,8 +42,12 @@ public class AlienAttackFrame extends JFrame {
         
         //Housekeeping
         readConfigFile();
+        gameFrameSize = 900;
         setTitle("Alien Attack [Alpha]");
         setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+        //set up game engine
+        gameTimer = new Timer(cycleTime, new GameEngine());
         commandQueue = new LinkedList<Integer>();
 
         //Controls - initialize and add
@@ -72,6 +79,8 @@ public class AlienAttackFrame extends JFrame {
         getContentPane().add(controlPanel, BorderLayout.SOUTH);
         getContentPane().add(optionPanel, BorderLayout.NORTH);
 
+        //Final housekeeping
+        gameTimer.start();
         pack();
     }
 
@@ -86,22 +95,39 @@ public class AlienAttackFrame extends JFrame {
                 configSettings.add(data[1]);
             }
             fileReader.close();
-            System.out.println(configSettings);
+            cycleTime = Integer.parseInt(configSettings.get(1));
+            largeAlienStartSpeed = Integer.parseInt(configSettings.get(2));
+            medAlienStartSpeed = Integer.parseInt(configSettings.get(3));
+            smallAlienStartSpeed = Integer.parseInt(configSettings.get(4));
+            playerSpeed = Integer.parseInt(configSettings.get(5));
+            increaseInterval = Integer.parseInt(configSettings.get(6));
+            startMaxAliens = Integer.parseInt(configSettings.get(7));
+            startMinAliens = Integer.parseInt(configSettings.get(8));
+            largeAlienValue = Integer.parseInt(configSettings.get(9));
+            medAlienValue = Integer.parseInt(configSettings.get(10));
+            smallAlienValue = Integer.parseInt(configSettings.get(11));
         } catch(IOException e) {
             JOptionPane.showMessageDialog(this, "Could not access config file. Starting game with default values.");
-            cycleTime = 200;
-            largeAlienStartSpeed = 20;
-            medAlienStartSpeed = 30;
-            smallAlienStartSpeed = 40;
-            playerSpeed = 40;
-            increaseInterval = 20;
-            increaseSize = 2;
-            startMaxAliens = 4;
-            startMinAliens = 1;
-            largeAlienValue = 50;
-            medAlienValue = 25;
-            smallAlienValue = 10;
+            setDefaultValues();
+        } catch(NumberFormatException e) {
+            JOptionPane.showMessageDialog(this, "Problem reading config file. Starting game with default values.");
+            setDefaultValues();
         }
+    }
+
+    public void setDefaultValues() {
+        cycleTime = 200;
+        largeAlienStartSpeed = 20;
+        medAlienStartSpeed = 30;
+        smallAlienStartSpeed = 40;
+        playerSpeed = 40;
+        increaseInterval = 20;
+        increaseSize = 2;
+        startMaxAliens = 4;
+        startMinAliens = 1;
+        largeAlienValue = 50;
+        medAlienValue = 25;
+        smallAlienValue = 10;
     }
 
     public class ControlListener implements ActionListener {
@@ -117,6 +143,26 @@ public class AlienAttackFrame extends JFrame {
             }
         }
 
+    }
+
+    public class GameEngine implements ActionListener {
+
+        public void actionPerformed(ActionEvent e) {
+            int currCommand;
+            try {
+                currCommand = commandQueue.remove();
+            } catch(NoSuchElementException i) {
+                currCommand = 2;
+            }
+
+            if(currCommand==0) {
+                System.out.println("MOVE LEFT");
+            } else if(currCommand==1) {
+                System.out.println("MOVE RIGHT");
+            } else {
+                System.out.println("DON'T MOVE");
+            }
+        }
     }
 
 }
